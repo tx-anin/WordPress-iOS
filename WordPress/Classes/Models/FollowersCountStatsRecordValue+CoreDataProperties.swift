3939903d7851beb3b655a58c5af6d435b079a854
Leandro Alonso <contato@leandroalonso.com>
2020-06-09 13:47:20 -0300

    Remove unused fetchRequest()

--- WordPress/Classes/Models/FollowersCountStatsRecordValue+CoreDataProperties.swift
+++ WordPress/Classes/Models/FollowersCountStatsRecordValue+CoreDataProperties.swift
@@ -4,10 +4,6 @@ import CoreData
 
 extension FollowersCountStatsRecordValue {
 
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<FollowersCountStatsRecordValue> {
-        return NSFetchRequest<FollowersCountStatsRecordValue>(entityName: "FollowersCountStatsRecordValue")
-    }
-
     @NSManaged public var type: Int16
     @NSManaged public var count: Int64
 
