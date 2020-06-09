a2b3a39f69990218531243578aa8b372d981d65d
Leandro Alonso <contato@leandroalonso.com>
2020-06-09 13:40:57 -0300

    Remove unused method

--- WordPress/Classes/Models/CountryStatsRecordValue+CoreDataProperties.swift
+++ WordPress/Classes/Models/CountryStatsRecordValue+CoreDataProperties.swift
@@ -4,10 +4,6 @@ import CoreData
 
 extension CountryStatsRecordValue {
 
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<CountryStatsRecordValue> {
-        return NSFetchRequest<CountryStatsRecordValue>(entityName: "CountryStatsRecordValue")
-    }
-
     @NSManaged public var countryCode: String?
     @NSManaged public var countryName: String?
     @NSManaged public var viewsCount: Int64
