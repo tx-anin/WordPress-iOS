2d5d2c9bfe51d2339934182ab67f45022db80821
Leandro Alonso <contato@leandroalonso.com>
2020-06-09 13:47:16 -0300

    Remove unused fetchRequest()

--- WordPress/Classes/Models/FileDownloadsStatsRecordValue+CoreDataProperties.swift
+++ WordPress/Classes/Models/FileDownloadsStatsRecordValue+CoreDataProperties.swift
@@ -3,10 +3,6 @@ import CoreData
 
 extension FileDownloadsStatsRecordValue {
 
-    @nonobjc public class func fetchRequest() -> NSFetchRequest<FileDownloadsStatsRecordValue> {
-        return NSFetchRequest<FileDownloadsStatsRecordValue>(entityName: "FileDownloadsStatsRecordValue")
-    }
-
     @NSManaged public var downloadCount: Int64
     @NSManaged public var file: String?
 
