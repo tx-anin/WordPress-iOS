d7e42f472ccb93de73d5a11b2eb77ae0b2b03dbc
Leandro Alonso <contato@leandroalonso.com>
2020-06-09 12:01:16 -0300

    Remove unused methods from Math.swift

--- WordPress/Classes/Extensions/Math.swift
+++ WordPress/Classes/Extensions/Math.swift
@@ -33,24 +33,6 @@ extension Comparable {
     }
 }
 
-extension CGSize {
-    func clamp(min minValue: CGSize, max maxValue: CGSize) -> CGSize {
-        let width = self.width.clamp(min: minValue.width, max: maxValue.width)
-        let height = self.height.clamp(min: minValue.height, max: maxValue.height)
-        return CGSize(width: width, height: height)
-    }
-
-    func clamp(min minValue: CGFloat, max maxValue: CGFloat) -> CGSize {
-        let minSize = CGSize(width: minValue, height: minValue)
-        let maxSize = CGSize(width: maxValue, height: maxValue)
-        return clamp(min: minSize, max: maxSize)
-    }
-
-    func clamp(min minValue: Int, max maxValue: Int) -> CGSize {
-        return clamp(min: CGFloat(minValue), max: CGFloat(maxValue))
-    }
-}
-
 extension CGFloat {
     func zeroIfNaN() -> CGFloat {
         return self.isNaN ? 0.0 : self
