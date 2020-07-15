1d98be01c5d73804c77d031a11f7a58e3d60105f
Diego Rey Mendez <diego@reymendez.com>
2020-07-15 10:22:41 -0300

    Removed a merge conflict marker.

--- WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
+++ WordPress/Classes/Utility/BuildInformation/FeatureFlag.swift
@@ -73,13 +73,8 @@ extension FeatureFlag: OverrideableFlag {
             return "Jetpack disconnect"
         case .debugMenu:
             return "Debug menu"
-<<<<<<< HEAD
         case .offlinePages:
             return "Offline Pages"
-        case .postReblogging:
-            return "Post Reblogging"
-=======
->>>>>>> develop
         case .unifiedAuth:
             return "Unified Auth"
         case .unifiedSiteAddress:
